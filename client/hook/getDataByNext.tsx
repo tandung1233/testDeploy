b74c674d4f91ced1898acbcc5e25c93a5a886f7d
tandung1233 <90715423+tandung1233@users.noreply.github.com>
2025-02-03 08:54:22 +0000

    12312312

--- client/hook/getDataByNext.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getDataByNext(url: string) {
-    const res = await fetch(url, {
-        cache: 'no-store', // Không cache để luôn lấy dữ liệu mới
-    });
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
-    }
-    return res.json();
-}
